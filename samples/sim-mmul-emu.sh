a5c68ff6be63b8453a5573327b58d9daeaf2febe
ubal <ubal@7c48a5b1-1362-454e-a514-e4da24589c60>
2012-06-05 13:14:47 +0000

    * Design of verification script interface * sim-mmul-emu.sh using verification interface.

--- samples/sim-mmul-emu.sh
+++ samples/sim-mmul-emu.sh
@@ -3,6 +3,7 @@
 M2S_CLIENT_KIT_PATH="m2s-client-kit"
 M2S_CLIENT_KIT_BIN_PATH="$M2S_CLIENT_KIT_PATH/bin"
 M2S_CLIENT_KIT_RESULT_PATH="$M2S_CLIENT_KIT_PATH/result"
+M2S_CLIENT_KIT_DOC_PATH="$M2S_CLIENT_KIT_PATH/doc"
 
 prog_name=`echo $0 | awk -F/ '{ print $NF }'`
 sim_cluster_sh="$HOME/$M2S_CLIENT_KIT_BIN_PATH/sim-cluster.sh"
@@ -20,27 +21,22 @@ function syntax()
 {
 	cat << EOF
 
-Syntax:
-    $prog_name <command> [<arguments>]
-
 Run a simulation for benchmark MatrixMultiplication in suite AMDAPP-2.5, using a
 functional emulation, and activating the self-check option in the benchmark.
 Square matrices are multiplied with sizes ranging between 16 x 16 and 1k x 1k.
 
-Possible values for <command> are:
+* Secondary verification scripts
+	None
 
-  run <server>[:<port>]
-      Create a cluster and submit it to a server, optionally specifying the SSH
-      port. Commands "sim-cluster.sh list/state/kill/import/remove" can be used
-      later to manage the created cluster.
+* Associated clusters
+	mmul-emu
 
-  process [-f]
-      Import output files of the cluster if they do not exist already locally,
-      and process them to generate plots, filter data, validate results, etc.
-      Optional flag '-f' forces the output files to be imported again from the
-      server, even if they already existed locally.
+--
 
 EOF
+
+	# Print verification script interface
+	cat $HOME/$M2S_CLIENT_KIT_DOC_PATH/verification-script-interface.txt
 	exit 1
 }
 
@@ -66,11 +62,29 @@ fi
 command=$1 ; shift
 
 # Process command
-if [ "$command" == run ]
+if [ "$command" == submit ]
 then
 
+	# Options
+	temp=`getopt -o r: -l configure-args: -n $prog_name -- "$@"`
+	[ $? == 0 ] || exit 1
+	eval set -- "$temp"
+	revision=
+	configure_args=
+	while true
+	do
+		case "$1" in
+		-r) revision=$2 ; shift 2 ;;
+		--configure-args) configure_args=$2 ; shift 2 ;;
+		--) shift ; break ;;
+		*) error "$1: invalid option" ;;
+		esac
+	done
+	[ -z "$revision" ] || revision_arg="-r $revision"
+	[ -z "$configure_args" ] || configure_args_arg="--configure-args \"$configure_arg\""
+
 	# Get argument
-	[ $# == 1 ] || error "invalid syntax for command"
+	[ $# == 1 ] || error "syntax: submit <server>[:<port>] [<options>]"
 	server_port=$1
 
 	# Create cluster
@@ -85,8 +99,27 @@ then
 
 	# Submit cluster
 	$sim_cluster_sh submit $cluster_name $server_port \
+		$revision_arg $configure_args_arg \
 		|| exit 1
 	
+elif [ "$command" == kill ]
+then
+
+	# Kill cluster
+	$sim_cluster_sh kill $cluster_name
+
+elif [ "$command" == state ]
+then
+
+	# Return state of cluster
+	$sim_cluster_sh state $cluster_name
+
+elif [ "$command" == wait ]
+then
+
+	# Wait for cluster
+	$sim_cluster_sh wait $cluster_name
+
 elif [ "$command" == process ]
 then
 
