4505950e285b59eacdd4877701a0786b5c6c1810
Rafael Ubal <rafaelubal@users.noreply.github.com>
2013-07-11 03:32:07 +0000

    Auto tests for ARM/MIPS disassembler/emulator

---
+++ local-tests/test-auto/mips-asm/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+M2S_CLIENT_KIT_PATH="$HOME/m2s-client-kit"
+M2S_CLIENT_KIT_TMP_PATH="$M2S_CLIENT_KIT_PATH/tmp"
+M2S_CLIENT_KIT_BUILD_PATH="$M2S_CLIENT_KIT_TMP_PATH/m2s-build"
+M2S="$M2S_CLIENT_KIT_BUILD_PATH/bin/m2s"
+
+# Run test
+$M2S --mips-disasm hello
+echo $?
+
