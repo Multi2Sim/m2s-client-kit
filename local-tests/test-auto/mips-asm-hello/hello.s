8aa07af441d48b4540989e5da35630ebdd0e1499
ubal <ubal@7c48a5b1-1362-454e-a514-e4da24589c60>
2013-07-11 16:22:01 +0000

    Auto tests * Fixed XXX-timing-mem-config * Fixed mips-asm-hello

--- local-tests/test-auto/mips-asm-hello/hello.s
+++ local-tests/test-auto/mips-asm-hello/hello.s
@@ -4,17 +4,12 @@ hello:		.asciz		"Hello, world!\n"	# a null terminated string
 		.align		4			# because of the way memory is built
 length:		.word		. - hello		# length = IC - (hello-addr)
 		.text					# begin code segment
-		.globl		main			# for gcc/ld linking
-		.ent		main			# for gdb debugging info.
-main:		# We must specify -non_shared to gcc or we`ll need these 3 lines that fallow.
-#		.set		noreorder		# disable instruction reordering
-#		.cpload		t9			# PIC ABI crap (function prologue)
-#		.set		reorder			# re-enable instruction reordering
-		move		a0,$0			# load stdout fd
-		la		a1,hello		# load string address
-		lw		a2,length		# load string length
-		li		v0,__NR_write		# specify system write service
+		.global		__start
+__start:	# We must specify -non_shared to gcc or we`ll need these 3 lines that fallow.
+		move		$a0, $0			# load stdout fd
+		la		$a1, hello		# load string address
+		lw		$a2, length		# load string length
+		li		$v0, 4004		# specify system write service
 		syscall					# call the kernel (write string)
-		li		v0,0			# load return code
-		j		ra			# return to caller
-		.end		main			# for dgb debugging info.
+		li		$v0, 4246		# Syscall 'exit_group'
+		syscall
