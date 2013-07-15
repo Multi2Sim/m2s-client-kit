cd3012a7af6fbc58c004a0cf2b748d5416b4473d
tushar.swamy <tushar.swamy@7c48a5b1-1362-454e-a514-e4da24589c60>
2013-07-15 02:06:43 +0000

    - Added Si assembler tests for BinomialOption, BitonicSort, and BlackScholes

---
+++ local-tests/test-auto/m2c-si2bin-native-BinomialOption/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+M2S_CLIENT_KIT_PATH="$HOME/m2s-client-kit"
+M2S_CLIENT_KIT_TMP_PATH="$M2S_CLIENT_KIT_PATH/tmp"
+M2S_CLIENT_KIT_BUILD_PATH="$M2S_CLIENT_KIT_TMP_PATH/m2s-build"
+M2C="$M2S_CLIENT_KIT_BUILD_PATH/bin/m2c"
+
+# Run test
+NAME="BinomialOption"
+DEVICE="pitcairn"
+
+$M2C --si-asm -m $DEVICE $NAME.s
+echo $?
+./$NAME --load $NAME.bin -e -q
+echo $?
+rm $NAME.bin
+
