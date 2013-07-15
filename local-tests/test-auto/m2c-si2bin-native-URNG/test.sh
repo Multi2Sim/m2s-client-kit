6acd2b87d5349e0caff5048d3ffbddea3bb31095
tushar.swamy <tushar.swamy@7c48a5b1-1362-454e-a514-e4da24589c60>
2013-07-15 03:34:29 +0000

    - Added SI Assembler tests for Reduction, ScanLargeArrays, SimpleConvolution, SobelFilter, and URNG

---
+++ local-tests/test-auto/m2c-si2bin-native-URNG/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+M2S_CLIENT_KIT_PATH="$HOME/m2s-client-kit"
+M2S_CLIENT_KIT_TMP_PATH="$M2S_CLIENT_KIT_PATH/tmp"
+M2S_CLIENT_KIT_BUILD_PATH="$M2S_CLIENT_KIT_TMP_PATH/m2s-build"
+M2C="$M2S_CLIENT_KIT_BUILD_PATH/bin/m2c"
+
+# Run test
+NAME="URNG"
+DEVICE="pitcairn"
+
+$M2C --si-asm -m $DEVICE $NAME.s
+echo $?
+./$NAME --load $NAME.bin -e -q
+echo $?
+rm $NAME.bin
+rm out[0-9].bmp
+
