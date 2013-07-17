3d1db6dc9b5d2981f3363bcf18d9fc79dbda7a29
Rafael Ubal <rafaelubal@users.noreply.github.com>
2013-07-17 21:55:35 +0000

    Manual tests for SI/EVG visualization

--- local-tests/test-manual/evg-visual/test.sh
+++ local-tests/test-manual/evg-visual/test.sh
@@ -6,32 +6,32 @@ M2S_CLIENT_KIT_BUILD_PATH="$M2S_CLIENT_KIT_TMP_PATH/m2s-build"
 M2S_CLIENT_KIT_TEST_AUTO_PATH="$M2S_CLIENT_KIT_PATH/local-tests/test-auto"
 M2S="$M2S_CLIENT_KIT_BUILD_PATH/bin/m2s"
 
-cat > si-config << EOF
+cat > evg-config << EOF
 [ Device ]
 NumComputeUnits = 4
 EOF
 
 name="matrix-mul"
-cp $M2S_CLIENT_KIT_TEST_AUTO_PATH/si-emu-matrix-mul/$name.bin .
-cp $M2S_CLIENT_KIT_TEST_AUTO_PATH/si-emu-matrix-mul/$name.c .
+cp $M2S_CLIENT_KIT_TEST_AUTO_PATH/evg-emu-matrix-mul/$name.bin .
+cp $M2S_CLIENT_KIT_TEST_AUTO_PATH/evg-emu-matrix-mul/$name.c .
 
 gcc $name.c -o $name \
 	-I$M2S_CLIENT_KIT_BUILD_PATH/runtime/include \
 	-L$M2S_CLIENT_KIT_BUILD_PATH/lib/.libs \
-	-lm2s-opencl \
+	-lm2s-opencl-old \
 	-m32
 
 
 LD_LIBRARY_PATH=$M2S_CLIENT_KIT_BUILD_PATH/lib/.libs \
-	M2S_OPENCL_BINARY=$name.bin \
-	$M2S \
-	--si-sim detailed \
-	--si-config si-config \
+$M2S \
+	--evg-sim detailed \
+	--evg-config evg-config \
+	--evg-kernel-binary $name.bin \
 	--trace trace.gz \
 	$name 40 10
 
 $M2S --visual trace.gz
 
 # Remove created files and finish
-rm -f $name $name.c $name.bin trace.gz si-config
+rm -f $name $name.c $name.bin trace.gz evg-config
 
