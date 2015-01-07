0e60d60cbe0d67f2833799bc333709fa918bcf4d
yifan.sun <yifan.sun@7c48a5b1-1362-454e-a514-e4da24589c60>
2015-01-07 04:18:24 +0000

    Test for HSA disassembler added

--- local-tests/test-auto/hsa-asm/test.sh
+++ local-tests/test-auto/hsa-asm/test.sh
@@ -1,13 +1,12 @@
-#!bin/bash
-set -e
+#!/bin/bash
 
 #First run the official disassembler provided by HSA foundation
-./hsailasm -assemble target_source.hsail
-mv target_source.brig target.brig
-./hsailasm --disassemble target.brig
+# ./hsailasm -assemble target_source.hsail
+# mv target_source.brig target.brig
+# ./hsailasm --disassemble target.brig
 
 # Second, run m2s to disassemble the brig file
-m2s --hsa-disasm target.brig > result.hsail
+$M2S --hsa-disasm target.brig > result.hsail
 
 # At last, compare the difference between the result and the target
 diff target.hsail result.hsail
