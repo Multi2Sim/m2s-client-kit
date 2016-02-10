3f6a6115dada7d6c8d3aa4fbb3f2982a60b3a4bd
Amir Kavyan <ziabari@gmail.com>
2016-02-09 19:20:57 -0500

    Adding the support for the other two options to the report as well

--- local-tests/test-auto/net-report/test.sh
+++ local-tests/test-auto/net-report/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # The test checks the stand alone network works without a problem.
-# it produces a report that should follow a certain format. 
+# it produces a report that should follow a certain format. It also
+# tests options --net-max-cycles and --net-msg-size 
 net_name=net0
-$M2S --net-sim $net_name --net-config net-config --net-report net_report
+$M2S --net-sim $net_name --net-config net-config --net-report net_report \
+	--net-max-cycles 200000 --net-msg-size 4
 echo $?
 cat $net_name\_net_report
 rm $net_name\_net_report
