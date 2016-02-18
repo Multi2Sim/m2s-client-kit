79b77aacd7bb010015d0ec877523999ab83a6af5
Amir Kavyan <ziabari@gmail.com>
2016-02-17 22:16:57 -0500

    options --kpl-debug-driver, and --kpl-debug-isa

---
+++ local-tests/test-auto/kpl-debug-driver/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run test
+cp $M2S_TEST_PATH/kpl-emu-vectoradd/vectoradd_m2s .
+$M2S --kpl-debug-driver debug vectoradd_m2s 1>/dev/null 2>&2
+echo $?
+cat debug
+rm -r vectoradd_m2s debug
