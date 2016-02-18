fd25ea2a21408b32e7328d56b7244441d8156825
Amir Kavyan <ziabari@gmail.com>
2016-02-17 22:37:29 -0500

    option mips-debug-ctx

---
+++ local-tests/test-auto/mips-debug-ctx/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run test
+cp $M2S_BUILD_PATH/samples/mips/test-args .
+$M2S --mips-debug-ctx debug_file test-args a b c 1>/dev/null 2>&1
+echo $?
+cat debug_file
+rm test-args debug_file
