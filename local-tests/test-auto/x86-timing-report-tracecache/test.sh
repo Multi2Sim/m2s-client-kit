4c74dfc3e26330bbc759729b5a2f204e1975c6c6
kavyan <ziabari@gmail.com>
2016-03-01 23:38:41 -0500

    new tests for options related to trace cache

---
+++ local-tests/test-auto/x86-timing-report-tracecache/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run test
+gcc test-args.c -o test-args -m32 || exit
+$M2S --x86-sim detailed \
+	--x86-report x86-report \
+	--x86-config x86-config \
+	test-args \
+	>/dev/null 2>/dev/null
+echo $?
+cat x86-report
+rm test-args
+rm x86-report
+
